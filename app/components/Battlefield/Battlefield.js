ae34cbac1112f733f08e9ae0a2b8fe2f27c105a2
Emma Ramstedt <ceramstedt@gmail.com>
2024-10-09 16:18:01 +0200

    init + battlefield component

---
+++ app/components/Battlefield/Battlefield.js
@@ -0,0 +1,67 @@
+
+import "./battlefield.scss"
+export default function Battlefield() {
+    return (
+      <div className="wrapper"><div className="honeycomb">
+      <div className="cell-row1">
+      <div className="cell-row1-inner">
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-1"><span>13</span></a></div>
+        </div>
+        </div>
+        </div>
+        <div className="cell-row2">
+        <div className="cell-row2-inner">
+        <div className="cell odd-row">
+          <div className="cell-inner"><a className="content cell-2"><span>12</span></a></div>
+        </div>
+        <div className="cell odd-row">
+          <div className="cell-inner odd-row"><a className="content cell-3"><span>11</span></a></div>
+        </div></div>
+        </div>
+        <div className="cell-row3">
+      <div className="cell-row3-inner">
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-4"><span>9</span></a></div>
+        </div></div></div>
+          <div className="cell-row4">
+      <div className="cell-row4-inner">
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-5"><span>10</span></a></div>
+        </div>
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-6"><span>8</span></a></div>
+        </div></div></div>
+            <div className="cell-row5">
+      <div className="cell-row5-inner">
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-7"><span>7</span></a></div>
+        </div>
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-8"><span>6</span></a></div>
+        </div></div></div>
+              <div className="cell-row6">
+      <div className="cell-row6-inner">
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-9"><span>5</span></a></div>
+        </div>
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-10"><span>4</span></a></div>
+        </div>
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-11"><span>3</span></a> </div></div></div>
+                  <div className="cell-row7">
+      <div className="cell-row7-inner">
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-12"><span>2</span></a></div>
+        </div>
+        <div className="cell">
+          <div className="cell-inner"><a className="content cell-13"><span>1</span></a></div></div></div>
+        </div>
+      </div>
+      </div>
+      </div>
+      
+    );
+  }
+  
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Battlefield from "./components/Battlefield/Battlefield";
+
+export default function Home() {
+  return (
+
+      <main >
+      <Battlefield/>
+    </main>
+  );
+}
